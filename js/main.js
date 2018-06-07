27bc7896cc804ba4f4bfb52e9f8566ec564e6d40
Andriy Koylyak <koylyakandriy@gmail.com>
2018-06-07 19:09:41 +0300

    add new pages

--- js/main.js
+++ js/main.js
@@ -1,7 +1,40 @@
-;(function($){
-	"use strict";
-	
+$(document).ready(function() {
+		var menuBtn = $('.top-nav_btn');
+		var menu = $('.top-nav_menu');
+
+		var sidebarBtn = $('.left-sidebar_btn');
+		var sidebarMenu = $('.left-sidebar_menu');
 
 
+		menuBtn.on('click', function(event) {
+			event.preventDefault();
+			menu.toggleClass('top-nav_menu__active');
+		});
 
-})(jQuery);
+		sidebarBtn.on('click', function(event) {
+			event.preventDefault();
+			sidebarMenu.toggleClass('left-sidebar_menu__active');
+		});
+		$('.direction-blocks').slick({
+		arrows: false,
+		dots: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		responsive: [
+		    {
+		      breakpoint: 992,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    },
+		     {
+		      breakpoint: 1200,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 1
+		      }
+		    },
+	    ]
+	});
+});
